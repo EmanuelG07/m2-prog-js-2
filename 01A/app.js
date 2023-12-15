9b79d00f663581d1150bd4f4ee1e85df7126c347
EmanuelG07 <144899298+EmanuelG07@users.noreply.github.com>
2023-12-15 11:50:37 +0100

    oke

--- 01A/app.js
+++ 01A/app.js
@@ -4,6 +4,18 @@ class App
     {
         console.log("hello world!")
         //code gaat hier!
+        let appNaam ="Mijn app";
+        console.log("Appnaam: Mijn app");
+        let versienummer =9;
+        console.log("Versienummer: 9");
+        let versiedatum =15-12-2023;
+        console.log("Versiedatum: 15-12-2023");
+        let autheur ="Emanuel Ghiorghita";
+        console.log("Autheur: Emanuel Ghiorghita");
+        let copyright ="Emanuel BV";
+        console.log("Copyright: Emanuel BV");
+        let darkmode =false
+        console.log("Darkmode: false")  
     }
 }
 
